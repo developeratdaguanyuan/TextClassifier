fed5df8f090f212c1ddde1a6912893be248b008a
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-03-30 23:51:38 -0500

    created models as modules

--- src/models/bidirectional_rnn.py
+++ src/models/bidirectional_rnn.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import reader
 
 class InputConfig(object):
   def __init__(self, _vocab_size, _class_size):
@@ -46,13 +45,6 @@ class RNNClassification(object):
     rnn_outputs_fw, rnn_outputs_bw = rnn_outputs
     last_output = tf.transpose(rnn_outputs_fw, perm=[1, 0, 2])[-1]
     
-    """
-    idx_fw = tf.range(self.batch_size) * tf.shape(rnn_outputs_fw)[1] + (self.seqlen - 1) # very important
-    last_rnn_output_fw = tf.gather(tf.reshape(rnn_outputs_fw, [-1, size]), idx_fw) # [batch_size, size]
-    idx_bw = tf.range(self.batch_size) * tf.shape(rnn_outputs_bw)[1] + (self.seqlen - 1) # very important
-    last_rnn_output_bw = tf.gather(tf.reshape(rnn_outputs_bw, [-1, size]), idx_bw) # [batch_size, size]
-    """
-
     # Softmax layer
     with tf.variable_scope('softmax'):
       W = tf.get_variable('W', [size, num_classes])
@@ -65,11 +57,11 @@ class RNNClassification(object):
     self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
 
   def train(self, train_data_producer, valid_data_producer):
-
+    self.saver = tf.train.Saver()
     with tf.Session() as sess:
       sess.run(tf.global_variables_initializer())
 
-      train_accuracy = 0
+      max_valid_accuracy, train_accuracy = 0, 0
       num_iteration = self.num_epoch * train_data_producer.size / self.batch_size
       for i in range(num_iteration):
         curr_data, curr_label, curr_seqlen = train_data_producer.next_batch(self.batch_size)
@@ -83,22 +75,12 @@ class RNNClassification(object):
             valid_data, valid_label, valid_seqlen = valid_data_producer.next_batch(self.batch_size)
             valid_feed = {self.x: valid_data, self.y: valid_label, self.seqlen: valid_seqlen}
             valid_accuracy += sess.run([self.accuracy], feed_dict=valid_feed)[0]
-          print("[train accuracy] %5.3f [valid accuracy] %5.3f"
-                % (train_accuracy / train_data_producer.size * self.batch_size, valid_accuracy / valid_iteration))
+
+          valid_accuracy /= valid_iteration
+          train_accuracy = train_accuracy / train_data_producer.size * self.batch_size
+          print("[train accuracy] %5.3f [valid accuracy] %5.3f" % (train_accuracy, valid_accuracy))
           train_accuracy = 0
-
-def main(_):
-  #train_data, valid_data, vocabulary, word_to_id, label_to_id = reader.ptb_raw_data("DSL-Task-master/data/DSLCC-v2.0/train-dev/")
-  train_data, valid_data, vocabulary, word_to_id, label_to_id \
-    = reader.ptb_raw_data("/home/dzhou/tfprojects/TextClassifier/data/20Newsgroups/")
-  train_data_producer = reader.DataProducer(train_data)
-  valid_data_producer = reader.DataProducer(valid_data)
-
-  model_config = ModelConfig()
-  input_config = InputConfig(vocabulary, len(label_to_id))
-  graph = RNNClassification(model_config, input_config)
- 
-  graph.train(train_data_producer, valid_data_producer)
-
-if __name__ == "__main__":
-  tf.app.run()
+          if valid_accuracy > max_valid_accuracy:
+            max_valid_accuracy = valid_accuracy
+            save_path = self.saver.save(sess, "/tmp/bidirectional_rnn")
+            print("Model saved in file: %s" % save_path)
