59e41f81dc523ba079f5f48d278d224309ba0d79
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-04-02 10:15:58 -0500

    created trainer and classifier

--- src/trainer.py
+++ src/trainer.py
@@ -7,9 +7,9 @@ def main(_):
     train_data, valid_data, vocabulary, word_to_id, label_to_id \
         = reader.ptb_raw_data("/home/dzhou/tfprojects/TextClassifier/data/20Newsgroups/")
     train_data_producer = reader.DataProducer(train_data)
-        valid_data_producer = reader.DataProducer(valid_data)
+    valid_data_producer = reader.DataProducer(valid_data)
 
-    model_config = birnn.ModelConfig()
+    model_config = birnn.ModelConfig(256, 100, 1e-3, 1000)
     input_config = birnn.InputConfig(vocabulary, len(label_to_id))
     graph = birnn.RNNClassification(model_config, input_config)
 
