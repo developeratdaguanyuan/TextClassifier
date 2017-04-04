0c5c070b0cbea7b1166f5f4e51e72b007458956e
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-04-04 08:19:34 -0500

    polish

--- src/models/rnn.py
+++ src/models/rnn.py
@@ -1,33 +1,30 @@
 import tensorflow as tf
 import reader
 
-class InputConfig(object):
-  def __init__(self, _vocab_size, _class_size):
-    self.vocab_size = _vocab_size
-    self.class_size = _class_size
 
 class ModelConfig(object):
-  hidden_size = 256
-  batch_size = 100
-  learning_rate = 1e-3
-  num_epoch = 1000
-  keep_prob = 0.5
+  def __init__(self, hidden_size, batch_size, learning_rate, vocab_size, class_size):
+    self.hidden_size = hidden_size
+    self.batch_size = batch_size
+    self.learning_rate = learning_rate
+    self.vocab_size = vocab_size
+    self.class_size = class_size
+    #self.keep_prob = keep_prob
 
-class RNNClassification(object):
-  def __init__(self, model_config, input_config):
-    self.num_epoch = model_config.num_epoch
+class RNNClassifier(object):
+  def __init__(self, model_config, is_training):
     self.batch_size = model_config.batch_size
 
     size = model_config.hidden_size
     learning_rate = model_config.learning_rate
-    vocab_size = input_config.vocab_size
-    num_classes = input_config.class_size
+    vocab_size = model_config.vocab_size
+    num_classes = model_config.class_size
 
     # Placeholders
     self.x = tf.placeholder(tf.int32, [self.batch_size, None]) # [batch_size, ?]
     self.seqlen = tf.placeholder(tf.int32, [self.batch_size])
     self.y = tf.placeholder(tf.int32, [self.batch_size])
-    keep_prob = tf.constant(model_config.keep_prob)
+    #keep_prob = tf.constant(model_config.keep_prob)
 
     # Embedding layer
     embeddings = tf.get_variable('embedding_matrix', [vocab_size, size])
@@ -56,13 +53,13 @@ class RNNClassification(object):
     self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = logits_t, labels = self.y))
     self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
 
-  def train(self, train_data_producer, valid_data_producer):
-
+  def train(self, train_data_producer, valid_data_producer, num_epoch):
+    self.saver = tf.train.Saver()
     with tf.Session() as sess:
       sess.run(tf.global_variables_initializer())
 
-      train_accuracy = 0
-      num_iteration = self.num_epoch * train_data_producer.size / self.batch_size
+      max_valid_accuracy, train_accuracy = 0, 0
+      num_iteration = num_epoch * train_data_producer.size / self.batch_size
       for i in range(num_iteration):
         curr_data, curr_label, curr_seqlen = train_data_producer.next_batch(self.batch_size)
         feed = {self.x: curr_data, self.y: curr_label, self.seqlen: curr_seqlen}
@@ -70,27 +67,17 @@ class RNNClassification(object):
 
         if i > 0 and i % (train_data_producer.size / self.batch_size) == 0:
           valid_accuracy = 0
-          valid_iteration = valid_data_producer.size/self.batch_size
+          valid_iteration = valid_data_producer.size / self.batch_size
           for j in range(valid_iteration):
             valid_data, valid_label, valid_seqlen = valid_data_producer.next_batch(self.batch_size)
             valid_feed = {self.x: valid_data, self.y: valid_label, self.seqlen: valid_seqlen}
             valid_accuracy += sess.run([self.accuracy], feed_dict=valid_feed)[0]
-          print("[train accuracy] %5.3f [valid accuracy] %5.3f"
-                % (train_accuracy / train_data_producer.size * self.batch_size, valid_accuracy / valid_iteration))
+
+          valid_accuracy /= valid_iteration
+          train_accuracy = train_accuracy / train_data_producer.size * self.batch_size
+          print("[train accuracy] %5.3f [valid accuracy] %5.3f" % (train_accuracy, valid_accuracy))
           train_accuracy = 0
-
-def main(_):
-  #train_data, valid_data, vocabulary, word_to_id, label_to_id = reader.ptb_raw_data("DSL-Task-master/data/DSLCC-v2.0/train-dev/")
-  train_data, valid_data, vocabulary, word_to_id, label_to_id \
-    = reader.ptb_raw_data("/home/dzhou/tfprojects/TextClassifier/data/20Newsgroups/")
-  train_data_producer = reader.DataProducer(train_data)
-  valid_data_producer = reader.DataProducer(valid_data)
-
-  model_config = ModelConfig()
-  input_config = InputConfig(vocabulary, len(label_to_id))
-  graph = RNNClassification(model_config, input_config)
- 
-  graph.train(train_data_producer, valid_data_producer)
-
-if __name__ == "__main__":
-  tf.app.run()
+          if valid_accuracy >= max_valid_accuracy:
+            max_valid_accuracy = valid_accuracy
+            save_path = self.saver.save(sess, "/tmp/rnn/rnn_" + str(i))
+            print("Model saved in file: %s" % save_path)
